103f4372cf0c01e7b80fc5dfaf7b9d778fc95abe
Ugenteraan <m.ugenteraan15@gmail.com>
2020-05-04 21:10:34 +0800

    argmax accuracy

--- darknet19.py
+++ darknet19.py
@@ -76,6 +76,7 @@ class Darknet19(NN.Module):
         Calculates the accuracy of the predictions.
         '''
         num_data = target.size()[0]
+        network_output = torch.argmax(network_output.contiguous()).view((num_data))
         correct_predictions = torch.sum(network_output==target)
         
         accuracy = (correct_predictions*100/num_data)
